dd62bf077a0974efa25ca02bd37f086f710544cd
Trevor Campbell <trevor.d.campbell@gmail.com>
2023-09-24 13:15:31 -0700

    initial commit

---
+++ diff-scroll.js
@@ -0,0 +1,31 @@
+
+var diffidx = 0;
+
+$(document).ready(function() {
+   var cur;
+   cur = $(".bd-main ins:visible,.bd-main del:visible").eq(diffidx)
+   cur.addClass("diff-selected");
+   $(cur)[0].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest"});
+});
+
+$(document).on("keypress", function(e) {
+   var cur;	
+   var next;
+   cur = $(".bd-main ins:visible,.bd-main del:visible").eq(diffidx)
+   cur.removeClass("diff-selected");
+   if (e.which == 110){
+     diffidx += 1;
+     if (diffidx > $(".bd-main ins:visible,.bd-main del:visible").length-1){
+         diffidx = $(".bd-main ins:visible,.bd-main del:visible").length-1;
+     }
+   } 
+   if (e.which == 78) {
+     diffidx -= 1;
+     if (diffidx < 0){
+     	diffidx = 0;
+     }
+   }
+   next = $(".bd-main ins:visible,.bd-main del:visible").eq(diffidx)
+   next.addClass("diff-selected")
+   $(next)[0].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest"});
+});
