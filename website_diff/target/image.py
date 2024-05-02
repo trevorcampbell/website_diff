8f3b212d00bfce64beb4b9673cbf4befeaf99c84
Brian Kim <brian.kim.2011@gmail.com>
2024-05-02 10:57:43 -0700

    Fix image gathering and diffing

--- website_diff/target/image.py
+++ website_diff/target/image.py
@@ -2,12 +2,12 @@ from PIL import Image, ImageEnhance, ImageChops, ImageOps
 import numpy as np
 
 def diff(filepath_old, filepath_new, filepath_out):
-    img_old = Image.open(filepath_old)
-    img_new = Image.open(filepath_new)
+    img_old = Image.open(filepath_old).convert("RGBA")
+    img_new = Image.open(filepath_new).convert("RGBA")
     img_diff = ImageChops.difference(img_old, img_new)
     if img_diff.getbbox() is None:
         # no diff, just save img_new to filepath_out
-        img_new.save(filepath_out)
+        img_new.convert("RGB").save(filepath_out)
         return False
     else:
         # diff, add a yellow border and highlight differences in bright red
@@ -16,7 +16,7 @@ def diff(filepath_old, filepath_new, filepath_out):
         img_overlay_np[ np.fabs(np.asarray(img_diff)).sum(axis=2) != 0 ] = np.array([255,0,0])
         img_overlay = Image.fromarray(img_overlay_np)
         img_bordered = ImageOps.expand(img_overlay, border=10, fill='yellow')
-        img_bordered.save(filepath_out)
+        img_bordered.convert("RGB").save(filepath_out)
         return True
 
 def highlight_add(filepath, filepath_out):
@@ -27,7 +27,7 @@ def highlight_del(filepath, filepath_out):
 
 def _highlight_image(filepath, filepath_out, color, alpha):
     img = Image.open(filepath)
-    bw = ImageEnhance.Color(img).enhance(0.0)
+    bw = ImageEnhance.Color(img).enhance(0.0).convert("RGBA")
     overlay = Image.new("RGBA", img.size, color = color)
     blended = Image.blend(bw, overlay, alpha)
-    blended.save(filepath_out)
+    blended.convert("RGB").save(filepath_out)
