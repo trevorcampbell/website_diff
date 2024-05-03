a30d4bc973e18c8b2f09a6f13d09fc7cd6d91543
Brian Kim <brian.kim.2011@gmail.com>
2024-05-03 14:04:48 -0700

    Fix image diffing and tests

--- website_diff/target/image.py
+++ website_diff/target/image.py
@@ -2,18 +2,18 @@ from PIL import Image, ImageEnhance, ImageChops, ImageOps
 import numpy as np
 
 def diff(filepath_old, filepath_new, filepath_out):
-    img_old = Image.open(filepath_old).convert("RGBA")
-    img_new = Image.open(filepath_new).convert("RGBA")
+    img_old = Image.open(filepath_old).convert("RGB")
+    img_new = Image.open(filepath_new).convert("RGB")
     img_diff = ImageChops.difference(img_old, img_new)
     if img_diff.getbbox() is None:
         # no diff, just save img_new to filepath_out
-        img_new.convert("RGB").save(filepath_out)
+        img_new.save(filepath_out)
         return False
     else:
         # diff, add a yellow border and highlight differences in bright red
-        img_overlay = ImageChops.overlay(img_old, img_new)
-        img_overlay_np = np.asarray(img_overlay)
-        img_overlay_np[ np.fabs(np.asarray(img_diff)).sum(axis=2) != 0 ] = np.array([255,0,0])
+        img_overlay = ImageChops.overlay(img_old.convert("RGBA"), img_new.convert("RGBA"))
+        img_overlay_np = np.array(img_overlay)
+        img_overlay_np[ np.fabs(np.array(img_diff)).sum(axis=2) != 0 ] = np.array([255,0,0,0])
         img_overlay = Image.fromarray(img_overlay_np)
         img_bordered = ImageOps.expand(img_overlay, border=10, fill='yellow')
         img_bordered.convert("RGB").save(filepath_out)
