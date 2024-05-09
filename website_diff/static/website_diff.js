e3ee3c7c5afc9679efc94c308c69c6dc48c7589b
Brian Kim <brian.kim.2011@gmail.com>
2024-05-09 13:13:08 -0700

    Skip to next off page diff class element

--- website_diff/static/website_diff.js
+++ website_diff/static/website_diff.js
@@ -8,24 +8,72 @@ $(document).ready(function() {
    $(cur)[0].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest"});
 });
 
+// Borrowed from https://stackoverflow.com/a/15203639
+function isElementVisible(el) {
+  var rect     = el.getBoundingClientRect(),
+      vWidth   = window.innerWidth || document.documentElement.clientWidth,
+      vHeight  = window.innerHeight || document.documentElement.clientHeight,
+      efp      = function (x, y) { return document.elementFromPoint(x, y) };     
+
+  // Return false if it's not in the viewport
+  if (rect.right < 0 || rect.bottom < 0 
+          || rect.left > vWidth || rect.top > vHeight)
+      return false;
+
+  // Return true if any of its four corners are visible
+  return (
+        el.contains(efp(rect.left,  rect.top))
+    ||  el.contains(efp(rect.right, rect.top))
+    ||  el.contains(efp(rect.right, rect.bottom))
+    ||  el.contains(efp(rect.left,  rect.bottom))
+  );
+}
+
 $(document).on("keypress", function(e) {
    var cur;	
    var next;
    cur = $(".diff:visible").eq(diffidx)
    cur.removeClass("diff-selected");
    if (e.which == 110){
-     diffidx += 1;
-     if (diffidx > $(".diff:visible").length-1){
-         diffidx = $(".diff:visible").length-1;
-     }
+    
+    var diffidx_tmp = diffidx + 1;
+    if (diffidx_tmp > $(".diff:visible").length-1){
+      diffidx_tmp = $(".diff:visible").length-1;
+    }
+    var cur_tmp = $(".diff:visible").eq(diffidx_tmp);
+
+    while (isElementVisible(cur_tmp[0])) {
+      diffidx_tmp += 1;
+      if (diffidx_tmp > $(".diff:visible").length-1){
+          diffidx_tmp = $(".diff:visible").length-1;
+          break
+      }
+      cur_tmp = $(".diff:visible").eq(diffidx_tmp)
+    }
+    cur = cur_tmp
+    diffidx = diffidx_tmp
    } 
    if (e.which == 78) {
-     diffidx -= 1;
-     if (diffidx < 0){
-     	diffidx = 0;
+     
+     var diffidx_tmp = diffidx - 1;
+     if (diffidx_tmp < 0){
+      diffidx_tmp = 0;
      }
+     var cur_tmp = $(".diff:visible").eq(diffidx_tmp);
+
+     while (isElementVisible(cur_tmp[0])) {
+      diffidx_tmp -= 1;
+      if (diffidx_tmp < 0){
+        diffidx_tmp = 0;
+        break
+      }
+      cur_tmp = $(".diff:visible").eq(diffidx_tmp)
+     }
+     cur = cur_tmp
+     diffidx = diffidx_tmp
    }
    next = $(".diff:visible").eq(diffidx)
    next.addClass("diff-selected")
    $(next)[0].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest"});
 });
+
