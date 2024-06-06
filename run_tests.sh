25acdb2208b387ad1bac3956b73c0e3b29b618d2
Brian Kim <brian.kim.2011@gmail.com>
2024-06-06 11:55:22 -0700

    Remove lines relating to temp dir

--- run_tests.sh
+++ run_tests.sh
@@ -6,13 +6,9 @@ fi
 
 examples_dir="website_diff_examples/examples"
 
-rm -rf $examples_dir/temp
 rm -rf $examples_dir/*/diff
 rm -rf $examples_dir/*/*/prerendered
-mkdir $examples_dir/temp
-cp -rf $examples_dir/* $examples_dir/temp
+rm -rf $examples_dir/*/prerendered_*
 
 for dir in $examples_dir/*; do [ -d "$dir" ] && [ "$dir" != "$examples_dir/temp" ] && website_diff --old $dir/old/ --new $dir/new/ --diff $dir/diff/; done
 
-cp -rf $examples_dir/temp/* $examples_dir
-rm -rf $examples_dir/temp
